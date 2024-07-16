06b36002ac0224389baea6593eda3fd65fe2506f
Joachim Alexandre Jasmin <joachim.jasmin@gmail.com>
2024-07-16 13:19:41 +0200

    chore: Update homepage content and links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -106,7 +106,7 @@ export default function Accueil() {
 				<div className="mx-auto max-w-7xl px-6 lg:px-8 mt-24">
 					<div className="mx-auto max-w-2xl text-center mb-16">
 						<h2 className="text-base font-semibold leading-7 text-indigo-600">100% personnalisé</h2>
-						<p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Parlons de votre projet</p>
+						<p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Parlons de vos besoins</p>
 						<p className="mt-6 text-lg leading-8 text-gray-600">
 							Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum pulvinar et feugiat blandit at. In
 							mi viverra elit nunc.
--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -3,15 +3,15 @@ import { HeartIcon } from "@heroicons/react/20/solid";
 
 const Footer = () => {
 	return (
-		<div className="mx-auto mt-12 px-6 lg:px-8 w-full">
-			<footer aria-labelledby="footer-heading" className="relative border-t border-gray-900/10 py-5 sm:mt-32 sm:py-10">
+		<div className="mx-auto px-6 lg:px-8 w-full">
+			<footer aria-labelledby="footer-heading" className="relative border-t border-gray-900/10 py-5 mt-32 sm:mt-16 sm:py-10">
 				<h2 id="footer-heading" className="sr-only">
 					Footer
 				</h2>
-				<div className="flex items-center justify-center gap-x-2">
-					<p>Créé avec </p>
-					<HeartIcon className="h-5" />
-					<p> par Joachim Jasmin pour Ipseis</p>
+				<div className="flex items-center justify-center gap-x-1">
+					<p className="text-sm">Créé avec </p>
+					<HeartIcon className="h-[14px]" />
+					<p className="text-sm"> par Joachim Jasmin pour Ipseis</p>
 				</div>
 			</footer>
 		</div>
