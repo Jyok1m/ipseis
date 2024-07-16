dc6ee9634a5ecaaaf5483e2f139f1daf1f79b6c9
Joachim Alexandre Jasmin <joachim.jasmin@gmail.com>
2024-07-16 19:57:53 +0200

    chore: added placeholders on inputs

--- src/components/home/ContactForm.tsx
+++ src/components/home/ContactForm.tsx
@@ -93,6 +93,7 @@ export default function ContactForm() {
 										name="first-name"
 										type="text"
 										autoComplete="given-name"
+										placeholder="ex. Jean"
 										className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 									/>
 								</div>
@@ -109,13 +110,14 @@ export default function ContactForm() {
 										name="last-name"
 										type="text"
 										autoComplete="family-name"
+										placeholder="ex. Dupont"
 										className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 									/>
 								</div>
 							</div>
 							<div>
 								<label htmlFor="budget" className="block text-sm font-semibold leading-6 text-gray-900">
-									Budget
+									Budget (€)
 								</label>
 								<div className="mt-2.5">
 									<input
@@ -124,6 +126,7 @@ export default function ContactForm() {
 										id="budget"
 										name="budget"
 										type="number"
+										placeholder="ex. 1500"
 										className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 									/>
 								</div>
@@ -139,6 +142,8 @@ export default function ContactForm() {
 										id="email"
 										name="email"
 										type="email"
+										autoComplete="email"
+										placeholder="ex. jean.dupont@jeandupont.fr"
 										className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 									/>
 								</div>
@@ -154,6 +159,7 @@ export default function ContactForm() {
 										id="message"
 										name="message"
 										rows={4}
+										placeholder="ex. Bonjour, je souhaiterais plus d'informations sur..."
 										className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 									/>
 								</div>
