be48f77fc1d5f619c3cafa162192ebf584e9f650
Joachim Alexandre Jasmin <joachim.jasmin@gmail.com>
2024-07-16 11:10:40 +0200

    chore: Add global Header and Footer components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
 "use client";
 
-import { useState } from "react";
-import { Dialog, DialogPanel } from "@headlessui/react";
-import { ArrowPathIcon, Bars3Icon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { ArrowPathIcon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon } from "@heroicons/react/24/outline";
 import { CheckIcon } from "@heroicons/react/20/solid";
 
-const navigation = [
-	{ name: "Product", href: "#" },
-	{ name: "Features", href: "#" },
-	{ name: "Marketplace", href: "#" },
-	{ name: "Company", href: "#" },
-];
 const features = [
 	{
 		name: "Push to deploy",
@@ -76,111 +68,14 @@ const faqs = [
 	},
 	// More questions...
 ];
-const footerNavigation = {
-	solutions: [
-		{ name: "Hosting", href: "#" },
-		{ name: "Data Services", href: "#" },
-		{ name: "Uptime Monitoring", href: "#" },
-		{ name: "Enterprise Services", href: "#" },
-	],
-	support: [
-		{ name: "Pricing", href: "#" },
-		{ name: "Documentation", href: "#" },
-		{ name: "Guides", href: "#" },
-		{ name: "API Reference", href: "#" },
-	],
-	company: [
-		{ name: "About", href: "#" },
-		{ name: "Blog", href: "#" },
-		{ name: "Jobs", href: "#" },
-		{ name: "Press", href: "#" },
-		{ name: "Partners", href: "#" },
-	],
-	legal: [
-		{ name: "Claim", href: "#" },
-		{ name: "Privacy", href: "#" },
-		{ name: "Terms", href: "#" },
-	],
-};
 
 function classNames(...classes: string[]) {
 	return classes.filter(Boolean).join(" ");
 }
 
 export default function Example() {
-	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
 	return (
 		<div className="bg-white">
-			{/* Header */}
-			<header className="absolute inset-x-0 top-0 z-50">
-				<nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
-					<div className="flex lg:flex-1">
-						<a href="#" className="-m-1.5 p-1.5">
-							<span className="sr-only">Your Company</span>
-							<img alt="" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" className="h-8 w-auto" />
-						</a>
-					</div>
-					<div className="flex lg:hidden">
-						<button
-							type="button"
-							onClick={() => setMobileMenuOpen(true)}
-							className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-						>
-							<span className="sr-only">Open main menu</span>
-							<Bars3Icon aria-hidden="true" className="h-6 w-6" />
-						</button>
-					</div>
-					<div className="hidden lg:flex lg:gap-x-12">
-						{navigation.map((item) => (
-							<a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
-								{item.name}
-							</a>
-						))}
-					</div>
-					<div className="hidden lg:flex lg:flex-1 lg:justify-end">
-						<a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-							Log in <span aria-hidden="true">&rarr;</span>
-						</a>
-					</div>
-				</nav>
-				<Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-					<div className="fixed inset-0 z-50" />
-					<DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-						<div className="flex items-center justify-between">
-							<a href="#" className="-m-1.5 p-1.5">
-								<span className="sr-only">Your Company</span>
-								<img alt="" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" className="h-8 w-auto" />
-							</a>
-							<button type="button" onClick={() => setMobileMenuOpen(false)} className="-m-2.5 rounded-md p-2.5 text-gray-700">
-								<span className="sr-only">Close menu</span>
-								<XMarkIcon aria-hidden="true" className="h-6 w-6" />
-							</button>
-						</div>
-						<div className="mt-6 flow-root">
-							<div className="-my-6 divide-y divide-gray-500/10">
-								<div className="space-y-2 py-6">
-									{navigation.map((item) => (
-										<a
-											key={item.name}
-											href={item.href}
-											className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-										>
-											{item.name}
-										</a>
-									))}
-								</div>
-								<div className="py-6">
-									<a href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-										Log in
-									</a>
-								</div>
-							</div>
-						</div>
-					</DialogPanel>
-				</Dialog>
-			</header>
-
 			<main className="isolate">
 				{/* Hero section */}
 				<div className="relative pt-14">
@@ -490,72 +385,6 @@ export default function Example() {
 					</div>
 				</div>
 			</main>
-
-			{/* Footer */}
-			<div className="mx-auto mt-32 max-w-7xl px-6 lg:px-8">
-				<footer aria-labelledby="footer-heading" className="relative border-t border-gray-900/10 py-24 sm:mt-56 sm:py-32">
-					<h2 id="footer-heading" className="sr-only">
-						Footer
-					</h2>
-					<div className="xl:grid xl:grid-cols-3 xl:gap-8">
-						<img alt="Company name" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" className="h-7" />
-						<div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
-							<div className="md:grid md:grid-cols-2 md:gap-8">
-								<div>
-									<h3 className="text-sm font-semibold leading-6 text-gray-900">Solutions</h3>
-									<ul role="list" className="mt-6 space-y-4">
-										{footerNavigation.solutions.map((item) => (
-											<li key={item.name}>
-												<a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
-													{item.name}
-												</a>
-											</li>
-										))}
-									</ul>
-								</div>
-								<div className="mt-10 md:mt-0">
-									<h3 className="text-sm font-semibold leading-6 text-gray-900">Support</h3>
-									<ul role="list" className="mt-6 space-y-4">
-										{footerNavigation.support.map((item) => (
-											<li key={item.name}>
-												<a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
-													{item.name}
-												</a>
-											</li>
-										))}
-									</ul>
-								</div>
-							</div>
-							<div className="md:grid md:grid-cols-2 md:gap-8">
-								<div>
-									<h3 className="text-sm font-semibold leading-6 text-gray-900">Company</h3>
-									<ul role="list" className="mt-6 space-y-4">
-										{footerNavigation.company.map((item) => (
-											<li key={item.name}>
-												<a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
-													{item.name}
-												</a>
-											</li>
-										))}
-									</ul>
-								</div>
-								<div className="mt-10 md:mt-0">
-									<h3 className="text-sm font-semibold leading-6 text-gray-900">Legal</h3>
-									<ul role="list" className="mt-6 space-y-4">
-										{footerNavigation.legal.map((item) => (
-											<li key={item.name}>
-												<a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
-													{item.name}
-												</a>
-											</li>
-										))}
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-				</footer>
-			</div>
 		</div>
 	);
 }
