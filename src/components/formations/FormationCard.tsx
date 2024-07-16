79a6724557c47f559dea1402442eee13da803656
Joachim Alexandre Jasmin <joachim.jasmin@gmail.com>
2024-07-16 14:57:25 +0200

    chore: Update SectionTitle and FormationCard components

---
+++ src/components/formations/FormationCard.tsx
@@ -0,0 +1,40 @@
+type FormationCardProps = {
+	title: string;
+	href: string;
+	description: string;
+	uri: string;
+};
+
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const FormationCard = (props: FormationCardProps) => {
+	return (
+		<article className="flex flex-col items-start justify-between">
+			<div className="relative w-full">
+				<Image
+					alt={props.title}
+					src={props.uri}
+					width={256}
+					height={256}
+					className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+				/>
+				<div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+			</div>
+			<div className="max-w-xl">
+				<div className="group relative">
+					<h3 className="mt-6 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+						<Link href={props.href}>
+							<span className="absolute inset-0" />
+							{props.title}
+						</Link>
+					</h3>
+					<p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{props.description}</p>
+				</div>
+			</div>
+		</article>
+	);
+};
+
+export default FormationCard;
--- src/components/global/SectionTitle.tsx
+++ src/components/global/SectionTitle.tsx
@@ -1,5 +1,5 @@
 type SectionTitleProps = {
-	tag: string;
+	tag?: string;
 	title: string;
 	description: string;
 };
@@ -9,7 +9,7 @@ import React from "react";
 const SectionTitle = ({ tag, title, description }: SectionTitleProps) => {
 	return (
 		<div className="mx-auto max-w-2xl text-center mb-16">
-			<h2 className="text-base font-semibold leading-7 text-indigo-600">{tag}</h2>
+			{tag && <h2 className="text-base font-semibold leading-7 text-indigo-600">{tag}</h2>}
 			<p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{title}</p>
 			<p className="mt-6 text-lg leading-8 text-gray-600">{description}</p>
 		</div>
