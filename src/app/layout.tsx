7f5a275fecdb16fd132428baa2042050eafc2409
Joachim Alexandre Jasmin <joachim.jasmin@gmail.com>
2024-08-22 15:53:13 +0200

    chore: Update fonts configuration and add lazy loading for images in ContactForm component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 
 import Header from "@/components/global/Header";
 import Footer from "@/components/global/Footer";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Ipseis",
@@ -19,8 +16,8 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body className={`${inter.className} flex flex-col justify-between min-h-screen`}>
+		<html lang="fr" className="font-serif">
+			<body className="flex flex-col justify-between min-h-screen">
 				<Header />
 				{children}
 				<Footer />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,10 @@ import type { Config } from "tailwindcss";
 const config: Config = {
 	content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
 	theme: {
+		fontFamily: {
+			serif: ["Halibut", "serif"],
+			sans: ["DM_Sans", "sans-serif"],
+		},
 		extend: {
 			backgroundImage: {
 				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
