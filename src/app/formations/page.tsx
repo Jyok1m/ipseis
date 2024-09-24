92b16b5a7897530c782876b7a8ccf79be25f824e
Joachim Alexandre Jasmin <joachim.jasmin@gmail.com>
2024-09-24 16:47:23 +0200

    Refactoriser la taille de police et les styles des formulaires de contact et des classes CSS sur la page d'accueil, la page de contact, la page de formation et le composant de notification

--- src/app/formations/page.tsx
+++ src/app/formations/page.tsx
@@ -64,7 +64,16 @@ export default function Formations() {
 	return (
 		<div className="bg-support py-8">
 			<div className="mx-auto max-w-7xl px-6 lg:px-8">
-				<SectionTitle title="Catalogue de Formations" description="Accompagner et prendre soin" />
+				<div className="mx-auto max-w-2xl text-center mb-16 text-univers">
+					<h2 className="text-lg font-semibold leading-6 text-cohesion">Accompagner et prendre soin</h2>
+					<h1 className="mt-2 text-4xl font-bold tracking-wider sm:text-4xl text-center">
+						Catalogue de <span className="text-cohesion">formations</span>
+					</h1>
+					<p className="mt-6 text-xl leading-6">
+						Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum pulvinar et feugiat blandit at. In
+						mi viverra elit nunc.
+					</p>
+				</div>
 				<div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
 					{formations.map((formation, index: number) => (
 						<FormationCard key={index} {...formation} />
