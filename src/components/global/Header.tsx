c6a64f4d0b58555a28a9a2c092c3e734d819fb32
Joachim Alexandre Jasmin <joachim.jasmin@gmail.com>
2025-03-19 12:47:57 +0100

    Ajouter un menu mobile et un lien "Contact" dans l'en-tête

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,27 +10,84 @@ const navigation = [
 	{ name: "Accueil", href: "/" },
 	{ name: "Formations", href: "/formations" },
 	{ name: "Organisme", href: "/a-propos" },
-	{ name: "Contact", href: "/contact" },
 ];
 
 const Header = () => {
+	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
 	return (
-		<header className="inset-x-0 top-0 z-50 bg-univers">
-			<nav aria-label="Global" className="flex flex-col items-center justify-center py-2 lg:p-6 gap-y-2 lg:gap-0">
-				<div className="block lg:absolute lg:left-6">
-					<Link href="/">
+		<header className="bg-univers">
+			<nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8">
+				<div className="flex lg:flex-1">
+					<Link href="/" className="-m-1.5 p-1.5">
 						<span className="sr-only">Ipseis</span>
 						<Image src={require("/src/_images/logo/logo_beige.svg")} alt="Logo Ipseis" title="Logo Ipseis" height={45} />
 					</Link>
 				</div>
-				<div className="flex gap-x-12 mb-5 lg:mb-0">
+				<div className="hidden md:flex md:gap-x-12">
 					{navigation.map((item) => (
-						<Link key={item.name} href={item.href} className="text-xl font-normal leading-6 text-support">
+						<Link key={item.name} href={item.href} className="text-md font-normal text-support">
 							{item.name}
 						</Link>
 					))}
 				</div>
+				<div className="flex flex-1 items-center justify-end gap-x-6">
+					<Link
+						href="/contact"
+						className="rounded-md bg-maitrise px-3 py-2 text-md text-support font-normal shadow-sm hover:bg-maitrise focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-maitrise"
+					>
+						Contactez-nous
+					</Link>
+				</div>
+				<div className="flex md:hidden">
+					<button
+						type="button"
+						onClick={() => setMobileMenuOpen(true)}
+						className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-support"
+					>
+						<span className="sr-only">Ouvrir le menu</span>
+						<Bars3Icon aria-hidden="true" className="size-6" />
+					</button>
+				</div>
 			</nav>
+
+			<Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="md:hidden">
+				<div className="fixed inset-0 z-10" />
+				<DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-maitrise px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-univers">
+					<div className="flex items-center gap-x-6">
+						<Link href="/" className="-m-1.5 p-1.5">
+							<span className="sr-only">Ipseis</span>
+							<Image src={require("/src/_images/logo/logo_beige.svg")} alt="Logo Ipseis" title="Logo Ipseis" height={32} />
+						</Link>
+
+						<Link
+							href="/contact"
+							className="ml-auto rounded-md bg-univers px-3 py-2 text-sm font-semibold text-support shadow-sm hover:bg-univers/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+						>
+							Contactez-nous
+						</Link>
+						<button type="button" onClick={() => setMobileMenuOpen(false)} className="-m-2.5 rounded-md p-2.5 text-univers">
+							<span className="sr-only">Fermer le menu</span>
+							<XMarkIcon aria-hidden="true" className="size-6" />
+						</button>
+					</div>
+					<div className="mt-6 flow-root">
+						<div className="-my-6 divide-y divide-gray-500/10">
+							<div className="space-y-2 py-6">
+								{navigation.map((item) => (
+									<Link
+										key={item.name}
+										href={item.href}
+										className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-univers hover:bg-support"
+									>
+										{item.name}
+									</Link>
+								))}
+							</div>
+						</div>
+					</div>
+				</DialogPanel>
+			</Dialog>
 		</header>
 	);
 };
