57ae735d2b9608cc2f346911fb35d9777479a157
Joachim Jasmin <118119984+Jyok1m@users.noreply.github.com>
2025-03-21 15:34:37 +0100

    Update Header.tsx

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -16,7 +16,7 @@ const Header = () => {
 	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
 	return (
-		<header className="bg-univers z-20">
+		<header className="bg-univers z-20 w-screen">
 			<nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8">
 				<div className="flex lg:flex-1">
 					<Link href="/" className="-m-1.5 p-1.5">
