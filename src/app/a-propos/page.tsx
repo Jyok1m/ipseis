356271bd33151fc9cfa7a9d384189391c2e68dbb
Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>
2025-08-29 12:51:37 +0200

    Add about image for Hélène to the public images directory

--- src/app/a-propos/page.tsx
+++ src/app/a-propos/page.tsx
@@ -12,49 +12,47 @@ import Image from "next/image";
 
 export default function APropos() {
 	return (
-		<div className="bg-support py-8">
-			<div className="mx-auto max-w-7xl px-6 lg:px-8">
-				<div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
-					<div className="lg:pl-20">
-						<div className="max-w-xs px-2.5 lg:max-w-none">
-							<Image
-								src="/images/about-image.jpg"
-								alt="Photo d'une femme en train de masser le pied d'une autre femme"
-								sizes="(min-width: 1024px) 32rem, 20rem"
-								width={1024}
-								height={2048}
-								className="rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
-							/>
-						</div>
-					</div>
-					<div className="lg:order-first lg:row-span-2">
-						<div className="text-4xl font-bold tracking-wider text-univers py-5">
-							<h1 className="mt-2 text-4xl font-bold tracking-wider sm:text-4xl uppercase">
-								Ipseis a été fondé dans le but de former les aidants de demain.
-							</h1>
-							<div className="border border-univers w-12 my-8"></div>
-						</div>
-						<div className="mt-6 space-y-7 text-xl text-zinc-600">
-							<p>
-								I’ve loved making things for as long as I can remember, and wrote my first program when I was 6 years old, just two weeks after my mom
-								brought home the brand new Macintosh LC 550 that I taught myself to type on.
-							</p>
-							<p>
-								The only thing I loved more than computers as a kid was space. When I was 8, I climbed the 40-foot oak tree at the back of our yard
-								while wearing my older sister’s motorcycle helmet, counted down from three, and jumped — hoping the tree was tall enough that with
-								just a bit of momentum I’d be able to get to orbit.
-							</p>
-							<p>
-								I spent the next few summers indoors working on a rocket design, while I recovered from the multiple surgeries it took to fix my badly
-								broken legs. It took nine iterations, but when I was 15 I sent my dad’s Blackberry into orbit and was able to transmit a photo back
-								down to our family computer from space.
-							</p>
-							<p>
-								Today, I’m the founder of Planetaria, where we’re working on civilian space suits and manned shuttle kits you can assemble at home so
-								that the next generation of kids really <em>can</em> make it to orbit — from the comfort of their own backyards.
-							</p>
-						</div>
-					</div>
+		<div className="bg-support py-8 h-full">
+			<div className="mx-auto max-w-7xl px-6 lg:px-8 pb-10">
+				<div className="text-2xl sm:text-4xl font-bold tracking-wider text-univers">
+					<h1 className="mt-2 font-bold tracking-wider uppercase">Bienvenue dans l’univers unique d’apprentissage d’IPSEIS</h1>
+					<div className="border border-univers w-12 my-8"></div>
+				</div>
+				<div>
+					<p className="space-y-7 text-xl text-zinc-600">
+						<Image
+							src="/images/about-image-hélène.jpg"
+							alt="Photo d'Hélène de Montabert"
+							sizes="(min-width: 1024px) 12rem, 8rem"
+							width={200}
+							height={200}
+							className="rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800 float-left mr-8 mb-4 max-w-xs"
+						/>
+						Madame, Monsieur, <br />
+						<br />
+						Face aux défis croissants du secteur sanitaire, social et médico-social (manque de ressources formées, turnover élevé, qualité des soins
+						inégale dans certains établissements; manque de motivation d’une partie des employés, stress, fatigue….), il est essentiel de repenser la
+						formation des professionnels et de leur apporter des outils appropriés pour réussir leurs missions. <br />
+						<br />
+						Depuis des années, j’observe un décalage entre les attentes des soignants, les formations proposées, leur efficacité et une motivation
+						inégale des équipes à apprendre avec les approches de formations qui leurs sont proposées. <br />
+						<br />
+						C’est pour répondre à ces enjeux que j’ai créés IPSEIS, un organisme de formation aux méthodes innovantes et spécialisé pour les
+						établissements sanitaires, sociaux et médico-sociaux. <br />
+						<br />
+						Parce que vous êtes uniques, nous proposons des formations sur mesure, basées sur une pédagogie active et immersive. Notre approche
+						expériencielle repose sur l’activité, la coopération, la participation et l’anticipation, avec des outils innovants tels que les SEGAPTM,
+						des « Sérieux Escape Games Apprenants & Pédagogiques » adaptés à votre contexte. <br />
+						<br />
+						Notre ambition : offrir des formations qui font la différence sur le terrain, en favorisant l’engagement et la montée en compétences des
+						soignants. <br />
+						<br />
+						Je suis à votre écoute pour construire, ensemble, des solutions adaptées à vos besoins. <br />
+						<br />
+						N’hésitez pas à me contacter ! <br />
+						<br />
+						Hélène de Montabert
+					</p>
 				</div>
 			</div>
 		</div>
