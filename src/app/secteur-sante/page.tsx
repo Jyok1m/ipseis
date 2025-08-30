7f49307ad904b2a73bc35251434592bae29c414d
Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>
2025-08-30 16:44:07 +0200

    Correction de la position de l'icône dans le composant FormatteurPoint pour un alignement amélioré et ajustement de la classe CSS pour une meilleure présentation.

--- src/app/secteur-sante/page.tsx
+++ src/app/secteur-sante/page.tsx
@@ -70,13 +70,14 @@ function FeatBox({
 function FormatteurPoint({ point }: { point: string }) {
 	return (
 		<p className="relative pl-10 text-base sm:text-xl tracking-wider leading-6 text-univers">
-			<span className="absolute left-0 top-1/2 transform -translate-y-1/2">
+			<span className="absolute left-0 -top-2">
+				{/* fixé en haut du paragraphe */}
 				<Image
 					src={require("../../_images/logo/star_orange.svg")}
 					alt="Check"
 					width={40}
 					height={40}
-					className="w-10 h-10 flex-shrink-0 object-contain"
+					className="w-10 h-10 flex-shrink-0 object-contain align-top"
 				/>
 			</span>
 			{point}
