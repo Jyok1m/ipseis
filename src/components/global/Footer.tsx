6aedea25a6b1c7ce63115e46388245e784ee17e9
Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>
2025-09-02 19:18:09 +0200

    Ajout de liens cliquables pour les noms des concepteurs dans le composant Footer pour une meilleure sémantique et accessibilité.

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import Link from "next/link";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
@@ -12,13 +13,23 @@ const Footer = () => {
 				aria-labelledby="footer-heading"
 				className="relative border-t border-gray-900/10 flex flex-col gap-y-2 sm:flex-row items-center justify-evenly py-5"
 			>
-				<span>Site web par Joachim Jasmin</span>
-				<span className="hidden sm:block">
-					Créé avec <FontAwesomeIcon icon={faHeart} className="h-3 sm:h-[14px] text-cohesion" /> pour Ipseis
+				<span>
+					Site web par{" "}
+					<Link href="https://www.linkedin.com/in/joachim-jasmin/" target="_blank" className="font-semibold hover:underline underline-offset-4">
+						Joachim Jasmin
+					</Link>
 				</span>
-				<span>Design par Titouan Gignouard</span>
-				<span className="block sm:hidden">
-					Créé avec <FontAwesomeIcon icon={faHeart} className="h-3 sm:h-[14px] text-cohesion" /> pour Ipseis
+				<span className="hidden sm:flex sm:justify-center sm:items-center sm:gap-x-2">
+					Créé avec <FontAwesomeIcon icon={faHeart} className="h-3 sm:h-[14px] text-cohesion -mb-1" /> pour Ipseis
+				</span>
+				<span>
+					Design par{" "}
+					<Link href="https://www.malt.fr/profile/titouanguignouard/" target="_blank" className="font-semibold hover:underline underline-offset-4">
+						Titouan Gignouard
+					</Link>
+				</span>
+				<span className="sm:hidden flex justify-center items-center gap-x-2">
+					Créé avec <FontAwesomeIcon icon={faHeart} className="h-3 sm:h-[14px] text-cohesion -mb-1" /> pour Ipseis
 				</span>
 			</footer>
 		</div>
