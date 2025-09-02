49fe9a6f6c8f40e50edb4bbcf110f8811f929e72
Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>
2025-09-02 17:17:01 +0200

    Ajout d'un modal pour les éléments de navigation non disponibles et amélioration de la gestion de l'état du menu mobile.

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -3,17 +3,19 @@
 import React, { useState } from "react";
 import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon, EnvelopeIcon } from "@heroicons/react/24/outline";
+import { Modal, ConfigProvider, Spin } from "antd";
 import Image from "next/image";
 import Link from "next/link";
 
 const navigation = [
-	{ name: "Nous connaître", href: "/a-propos" },
-	{ name: "Secteur santé", href: "/secteur-sante" },
-	{ name: "Autres secteurs", href: "/" },
+	{ name: "Nous connaître", href: "/a-propos", ready: true },
+	{ name: "Secteur santé", href: "/secteur-sante", ready: true },
+	{ name: "Autres secteurs", href: "/", ready: false },
 ];
 
 const Header = () => {
 	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+	const [showModal, setShowModal] = useState(false);
 
 	return (
 		<header className="bg-univers z-20 w-screen">
@@ -27,11 +29,21 @@ const Header = () => {
 					</Link>
 				</div>
 				<div className="hidden md:flex md:gap-x-12">
-					{navigation.map((item) => (
-						<Link key={item.name} href={item.href} className="text-lg font-normal text-support hover:underline hover:underline-offset-8">
-							{item.name}
-						</Link>
-					))}
+					{navigation.map((item) =>
+						item.ready ? (
+							<Link key={item.name} href={item.href} className="text-lg font-normal text-support hover:underline hover:underline-offset-8">
+								{item.name}
+							</Link>
+						) : (
+							<button
+								key={item.name}
+								onClick={() => setShowModal(true)}
+								className="text-lg font-normal text-support hover:underline hover:underline-offset-8"
+							>
+								{item.name}
+							</button>
+						)
+					)}
 				</div>
 				<div className="flex flex-1 items-center justify-end gap-x-6">
 					<Link
@@ -86,21 +98,60 @@ const Header = () => {
 					<div className="mt-6 flow-root">
 						<div className="-my-6 divide-y divide-gray-500/10">
 							<div className="space-y-2 py-6">
-								{navigation.map((item) => (
-									<Link
-										key={item.name}
-										href={item.href}
-										onClick={() => setMobileMenuOpen(false)}
-										className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold text-univers hover:bg-support"
-									>
-										{item.name}
-									</Link>
-								))}
+								{navigation.map((item) =>
+									item.ready ? (
+										<Link
+											key={item.name}
+											href={item.href}
+											onClick={() => setMobileMenuOpen(false)}
+											className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold text-univers hover:bg-support"
+										>
+											{item.name}
+										</Link>
+									) : (
+										<button
+											key={item.name}
+											onClick={() => setShowModal(true)}
+											className="-ml-3 rounded-lg px-3 py-2 text-base font-semibold text-univers hover:bg-support w-full flex"
+										>
+											{item.name}
+										</button>
+									)
+								)}
 							</div>
 						</div>
 					</div>
 				</DialogPanel>
 			</Dialog>
+
+			{/* Modal */}
+
+			<ConfigProvider
+				theme={{
+					components: {
+						Modal: {
+							titleFontSize: 18,
+							titleColor: "#263c27",
+							headerBg: "#fffce8",
+							contentBg: "#fffce8",
+						},
+					},
+					token: {
+						fontFamily: "Halibut",
+					},
+				}}
+			>
+				<Modal
+					title={<p className="text-lg sm:text-xl font-bold tracking-wider text-univers">Un peu de patience...</p>}
+					centered
+					open={showModal}
+					footer={null}
+					width={600}
+					onCancel={() => setShowModal(false)}
+				>
+					<p className="text-base sm:text-lg text-univers">Cette page sera bientôt disponible !</p>
+				</Modal>
+			</ConfigProvider>
 		</header>
 	);
 };
