2ac1c6ecac744d249c736b8fd59977c4fec84174
Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>
2025-09-03 19:35:59 +0200

    Refactor code structure for improved readability and maintainability

--- src/app/pedagogie/page.tsx
+++ src/app/pedagogie/page.tsx
@@ -116,7 +116,7 @@ function FeatBox({
 	className,
 	centered,
 }: {
-	title: string;
+	title?: string;
 	description: React.ReactNode;
 	bgColor: "support" | "maitrise";
 	className?: string;
@@ -128,7 +128,7 @@ function FeatBox({
 				bgColor === "support" ? "maitrise" : "support"
 			} h-full text-center flex flex-col justify-center ${className || ""}`}
 		>
-			<p className="mt-2 text-lg sm:text-xl font-bold tracking-tight items-center">{title}</p>
+			{title && <p className="mt-2 text-lg sm:text-xl font-bold tracking-tight items-center">{title}</p>}
 			<span className={`mt-2 text-base sm:text-lg ${centered ? "text-center" : "text-left"}`}>{description}</span>
 		</div>
 	);
@@ -232,7 +232,187 @@ export default function Pedagogie() {
 				</div>
 			</div>
 
+			{/* Objectifs pédagogiques */}
+
+			<Divider />
+			<TitleSection tag="Une formation adaptée et optimisée" title="Un processus efficace, fluide, collaboratif et de proximité" />
+			<div className="pb-10 mx-auto max-w-7xl px-6 lg:px-8 tracking-wider sm:mt-24">
+				<div className="mx-auto max-w-5xl">
+					<div className="relative grid grid-cols-1 gap-4 sm:gap-2 sm:[grid-template-columns:0.9fr_1fr_0.9fr] sm:[grid-template-rows:0.9fr_1fr_0.9fr] place-items-stretch">
+						<div className="sm:col-start-2 sm:row-start-1 sm:-translate-y-24">
+							<FeatBox
+								description={
+									<div className="relative pl-10">
+										<span className="absolute left-0 -top-[12px]">
+											<Image
+												src="/images/objectifs_peda/1.png"
+												alt="Check"
+												width={40}
+												height={40}
+												className="w-11 h-11 flex-shrink-0 object-contain align-top"
+											/>
+										</span>
+										<p>Analyse efficace de vos besoins & du contexte</p>
+									</div>
+								}
+								bgColor="support"
+								centered
+							/>
+						</div>
+
+						<div className="sm:col-start-3 sm:row-start-1 sm:translate-y-14">
+							<FeatBox
+								description={
+									<div className="relative pl-10">
+										<span className="absolute left-0 -top-[12px]">
+											<Image
+												src="/images/objectifs_peda/2.png"
+												alt="Check"
+												width={40}
+												height={40}
+												className="w-11 h-11 flex-shrink-0 object-contain align-top"
+											/>
+										</span>
+										<p>Proposition détaillée avec objectifs pédagogiques validés</p>
+									</div>
+								}
+								bgColor="support"
+								centered
+							/>
+						</div>
+
+						<div className="sm:col-start-3 sm:row-start-2 sm:-translate-x-2 sm:translate-y-14">
+							<FeatBox
+								description={
+									<div className="relative pl-10">
+										<span className="absolute left-0 -top-[12px]">
+											<Image
+												src="/images/objectifs_peda/3.png"
+												alt="Check"
+												width={40}
+												height={40}
+												className="w-11 h-11 flex-shrink-0 object-contain align-top"
+											/>
+										</span>
+										<p>Questionnaire de pré-formation envoyé aux apprenants</p>
+									</div>
+								}
+								bgColor="support"
+								centered
+							/>
+						</div>
+
+						<div className="sm:col-start-3 sm:row-start-3 sm:-translate-x-24 md:-translate-x-32 lg:-translate-x-36 sm:translate-y-14">
+							<FeatBox
+								description={
+									<div className="relative pl-10">
+										<span className="absolute left-0 -top-[12px]">
+											<Image
+												src="/images/objectifs_peda/4.png"
+												alt="Check"
+												width={40}
+												height={40}
+												className="w-11 h-11 flex-shrink-0 object-contain align-top"
+											/>
+										</span>
+										<p>Envoi d'une fiche pédagogique + informations pratiques</p>
+									</div>
+								}
+								bgColor="support"
+								centered
+							/>
+						</div>
+
+						<div className="sm:col-start-1 sm:row-start-3 sm:translate-x-24 md:translate-x-32 lg:translate-x-36 sm:translate-y-14">
+							<FeatBox
+								description={
+									<div className="relative pl-10">
+										<span className="absolute left-0 -top-[12px]">
+											<Image
+												src="/images/objectifs_peda/5.png"
+												alt="Check"
+												width={40}
+												height={40}
+												className="w-11 h-11 flex-shrink-0 object-contain align-top"
+											/>
+										</span>
+										<p>Formation active et immersive</p>
+									</div>
+								}
+								bgColor="support"
+								centered
+							/>
+						</div>
+
+						<div className="sm:col-start-1 sm:row-start-2 sm:translate-x-2 sm:translate-y-14">
+							<FeatBox
+								description={
+									<div className="relative pl-10">
+										<span className="absolute left-0 -top-[12px]">
+											<Image
+												src="/images/objectifs_peda/6.png"
+												alt="Check"
+												width={40}
+												height={40}
+												className="w-11 h-11 flex-shrink-0 object-contain align-top"
+											/>
+										</span>
+										<p>Questionnaire post-formation</p>
+									</div>
+								}
+								bgColor="support"
+								centered
+							/>
+						</div>
+
+						<div className="sm:col-start-1 sm:row-start-1 sm:translate-y-14">
+							<FeatBox
+								description={
+									<div className="relative pl-10">
+										<span className="absolute left-0 -top-[12px]">
+											<Image
+												src="/images/objectifs_peda/7.png"
+												alt="Check"
+												width={40}
+												height={40}
+												className="w-11 h-11 flex-shrink-0 object-contain align-top"
+											/>
+										</span>
+										<p>Débrief entre IPSEIS et la structure</p>
+									</div>
+								}
+								bgColor="support"
+								centered
+							/>
+						</div>
+
+						<div className="sm:col-start-2 sm:row-start-2 justify-center items-center relative z-10 hidden sm:flex">
+							<div className="absolute z-20 sm:-mt-8 sm:-mb-8 sm:-mx-6">
+								<Image
+									src={require("../../_images/logo/star_orange.svg")}
+									alt="Image de l'étoile d'Ipseis"
+									width={48}
+									height={48}
+									className="w-96 h-96"
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
 			{/* <Divider /> */}
+			<Divider />
+			<Divider />
+			<Divider />
+			<Divider />
+			<Divider />
+			<Divider />
+			<Divider />
+			<Divider />
+			<Divider />
+			<Divider />
+			<Divider />
 			<TitleSection
 				tag="Notre approche"
 				title="Choisir Ipseis c’est apprendre autrement pour progresser durablement"
