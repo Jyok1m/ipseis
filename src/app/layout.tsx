49075082bc4272d11e4f024f238418850856a4da
Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>
2026-02-17 12:56:24 +0100

    added speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import ConditionalHeader from "@/components/global/ConditionalHeader";
 import { defaultOpenGraph, defaultTwitter } from "@/components/utils/seo";
 
